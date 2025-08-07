44efada55df60baba5783aa1840e88c497332455
Anton <datskiiak@yandex.ru>
2025-08-07 09:53:21 +0300

    Fix length phone number

--- api/database/migrations/0001_01_01_000000_create_users_table.php
+++ api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('pernumber', 9)->unique();
-            $table->string('phone', 15)->unique()->nullable();
+            $table->string('phone', 11)->unique()->nullable();
             $table->string('name');
             $table->string('password');
             $table->string('role')->default('student');
