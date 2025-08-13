45868836122e5a35df9182827e2b3c88277aee6b
Anton <datskiiak@yandex.ru>
2025-08-13 12:52:22 +0300

    [CREATE] Middleware for role users

--- api/routes/api.php
+++ api/routes/api.php
@@ -8,5 +8,5 @@ use App\Http\Controllers\ReservationSlotController;
 Route::get('/profile', [ProfileController::class, 'profile'])->middleware('auth');
 Route::get('/slots', [SlotController::class, 'list'])->middleware('auth');
 
-Route::post('/slots/{slotId}/reserve', [ReservationSlotController::class, 'reserveSlotForSelf'])->middleware('auth');
-Route::put('/slots/{slotId}/cancel', [ReservationSlotController::class, 'cancelReservation'])->middleware('auth');
+Route::post('/slots/{slotId}/reserve', [ReservationSlotController::class, 'reserveSlotForSelf'])->middleware('role:student');
+Route::put('/slots/{slotId}/cancel', [ReservationSlotController::class, 'cancelReservation'])->middleware('role:student');
