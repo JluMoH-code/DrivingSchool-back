0f41e8d1458b7090275623e2d128165d68e7e1c2
Anton <datskiiak@yandex.ru>
2025-08-13 10:06:42 +0300

    [UPDATE] Logic return slots

--- api/app/Providers/SlotService.php
+++ api/app/Providers/SlotService.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Enums\SlotStatus;
 use App\Models\Slot;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class SlotService
@@ -11,7 +13,12 @@ class SlotService
 
     public function list(Request $request)
     {
-        $slots = Slot::all();
-        return $slots;
+        $query = Slot::select();
+
+        $query->where('start_time', '>=', Carbon::now());
+        $query->whereNot('status', SlotStatus::BLOCKED);
+        $query->orderBy('start_time');
+
+        return $query->get();
     }
 }
